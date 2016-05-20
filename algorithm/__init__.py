09ad7a8817cd59f8f4e99a56c772afb184382bb6
enihsyou <s1131234@gmail.com>
2016-05-20 23:59:01 +0800

    把算法文件放进一个包里

---
+++ algorithm/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+File name: __init__.py
+Reference: 
+Introduction: 
+Time: 2016-05-20
+Last modified: 2016-05-20
+By: enihsyou
+"""
+from .bubble_sort import bubble_sort_debug
+
+__all__ = ["bubble_sort"]
