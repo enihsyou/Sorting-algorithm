557dd840e7e1d473df9ef696bb6a975d37b66f85
九条涼果 <s1131234@gmail.com>
2017-04-13 12:10:22 +0800

    sync

--- Algorithms/src/main/java/com/enihsyou/algorithms/sort/SelectionSort.java
+++ Algorithms/src/main/java/com/enihsyou/algorithms/sort/SelectionSort.java
@@ -1,5 +1,50 @@
 package com.enihsyou.algorithms.sort;
 
-public class SelectionSort{
+import org.jetbrains.annotations.NotNull;
 
+public class SelectionSort implements Sorter {
+    @Override
+    public <T> Comparable<T>[] sort(final Comparable<T>[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (Sorter.less(list, j, min_index)) { min_index = j; }
+            }
+            Sorter.swap(list, i, min_index);
+        }
+        return list;
+    }
+
+    @Override
+    public double[] sort(@NotNull final double[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (Sorter.less(list, j, min_index)) { min_index = j; }
+            }
+            Sorter.swap(list, i, min_index);
+        }
+        return list;
+    }
+
+    @Override
+    public int[] sort(@NotNull final int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (Sorter.less(list, j, min_index)) { min_index = j; }
+            }
+            Sorter.swap(list, i, min_index);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort sorter = new SelectionSort();
+        final int size = args.length == 1 ? Integer.parseInt(args[0]) : 40;
+        System.out.format("%d integers%n", size);
+        Sorter.printResult(sorter.sort(Sorter.generateRandomIntegers(size)));
+        System.out.format("%d doubles%n", size);
+        Sorter.printResult(sorter.sort(Sorter.generateRandomDoubles(size)));
+    }
 }
