bc5dbe5172ab46b7d8de767aee33818a257b384e
九条涼果 <s1131234@gmail.com>
2017-11-12 14:24:24 +0800

    第6次作业： 1）键盘输入一个含有括号的四则运算表达式，可能含有多余的括号。编写一个算法整理该表达式， 去掉所有多余的括号。原表达式中所有变量和运算符相对位置保持不变，并保持与原表达式等价。 例：输入表达式 应输出表达式

--- Java_part/Assigment6/Q1_Test/Q1.py
+++ Java_part/Assigment6/Q1_Test/Q1.py
@@ -1,14 +1,18 @@
-# -*- codine: UTF-8 -*-
+# -*- coding: UTF-8 -*-
+"""
+第6次作业：
+1）键盘输入一个含有括号的四则运算表达式，可能含有多余的括号。编写一个算法整理该表达式，
+去掉所有多余的括号。原表达式中所有变量和运算符相对位置保持不变，并保持与原表达式等价。
+例：输入表达式 应输出表达式
+a+(b+c) a+b+c
+(a*b)+c/d a*b+c/d
+a+b/(c-d) a+b/(c-d)
+"""
 from random import choice, random
-from time import sleep
-
-import binarytree
 import string
 
-import sys
-
 OPERATORS = ('+', '-', '*', '/')
-NUMBERS = string.ascii_lowercase
+NUMBERS = string.ascii_letters + string.digits
 P = 0.5
 
 
@@ -71,10 +75,10 @@ def make_node(parent: Node, depth: int):
         parent.right = Node(generate_number())
 
 
-def get_precedence(char:str)->int:
-    if char =='+' or char == '-':
+def get_precedence(char: str) -> int:
+    if char == '+' or char == '-':
         return 1
-    if char =='*' or char == '/':
+    if char == '*' or char == '/':
         return 2
     return 0
 
@@ -88,7 +92,8 @@ def get_lowest(dic: dict) -> int:
     return 0
 
 
-def remove_parentheses(s: str, begin: int = 0, _stack=[]):
+# noinspection PyDefaultArgument
+def remove_parentheses(s: str, begin: int = 0, _stack=[]):  # _stack作为函数的域变量，记录括号匹配情况
     left_side_operator = None  # 记录在当前处理字符之前遇到的那个运算符
     local_operators = {k: 0 for k in OPERATORS}  # 记录当前括号上下文中操作符的数量
     pointer = begin
@@ -104,7 +109,7 @@ def remove_parentheses(s: str, begin: int = 0, _stack=[]):
         elif char == '(':  # 遇到左括号 利用调用栈保存当前状态，进入下一个括号的范围
             _stack.append(pointer)
             inner_operators, pointer, l_begin, pp = remove_parentheses(s, pointer)
-            remove += pp
+            remove += pp  # pp是临时变量 只是保存个返回结果
             """
             we know that parentheses are needed when at least one of the following conditions is true:
             
@@ -112,7 +117,7 @@ def remove_parentheses(s: str, begin: int = 0, _stack=[]):
             inside the parenthesized expression either to the left or to the right of the parenthesized expression.
             - There is a - operator to the left of the parenthesized expression, 
             and the parenthesized expression contains at least a + or -.
-            - There is a / operator to the left of the parenthesized expression
+            - There is a / operator to the left of the parenthesized expression.
             """
             if left_side_operator is not None \
                     and (left_side_operator == '-' and inner_operators['+'] + inner_operators['-']
@@ -126,38 +131,55 @@ def remove_parentheses(s: str, begin: int = 0, _stack=[]):
         elif char == ')':  # 从当前括号环境中跳出来返回上一个括号环境，返回当前括号中的符号统计
             try:
                 _stack.pop()
-            except IndexError as e:
-                raise ValueError("括号不匹配") from e
+            except IndexError as error:
+                raise ValueError("右括号不匹配") from error
             return local_operators, pointer, begin, remove
         else:
             raise NotImplementedError(char)
-    for i, c in enumerate(s):
-        if i not in remove:
-            print(c, end='')
-    print(flush=True)
+    if len(_stack) != 0 or len(remove) % 2 != 0:
+        raise ValueError("'%s'的左括号不匹配" % s)
+    # for i in sorted(remove, reverse=True):
+    #     del s[i]  # Not work
+    result = "".join([c for i, c in enumerate(s) if i not in remove])
+    return result
 
 
-remove_parentheses("(((q-o)*d)/i)")
-remove_parentheses("((x-m)-f)")
-
-my_null = None
-binarytree.customize(
-        node_init=lambda val: Node(val, my_null, my_null),
-        node_class=Node,
-        null_value=my_null,
-        value_attr='value',
-        left_attr='left',
-        right_attr='right'
-)
-while 1:
-    root = Node(generate_operator())
-    make_node(root, 0)
-    # root.print()
-    s = root.str()
-    print(s)
-    remove_parentheses(s)
-    sys.stdout.flush()
-    # my_custom_tree = root
-    # binarytree.show(my_custom_tree)
-
-    sleep(1)
+"""测试代码"""
+# remove_parentheses("(((q-o)*d)/i)")
+# remove_parentheses("((x-m)-f)")
+# try:
+#     import binarytree
+# except ImportError:
+#     pass
+# from time import sleep
+# import sys
+# my_null = None
+# binarytree.customize(
+#         node_init=lambda val: Node(val, my_null, my_null),
+#         node_class=Node,
+#         null_value=my_null,
+#         value_attr='value',
+#         left_attr='left',
+#         right_attr='right'
+# )
+# while 1:
+#     root = Node(generate_operator())
+#     make_node(root, 0)
+#     # root.print()
+#     s = root.str()
+#     print(s)
+#     remove_parentheses(s)
+#     sys.stdout.flush()
+#     # my_custom_tree = root
+#     # binarytree.show(my_custom_tree)
+#
+#     sleep(1)
+if __name__ == '__main__':
+    while 1:
+        s = input("输入算式: ")
+        try:
+            r = remove_parentheses(s)
+        except ValueError as e:
+            print(e)
+        else:
+            print(r)
